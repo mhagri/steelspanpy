d986ed2abe629613c83a683b603464100f0862c0
mhagri <m.hakan@gmail.com>
2026-04-10 14:40:50 +0300

    Delete steelspanpy/utils/__init__.py

--- steelspanpy/utils/__init__.py
+++
@@ -1,6 +0,0 @@
-from .geometry import first_hall, mid_columns, mid_braces, vertical_braces, vertical_stability, roof_braces, Grid
-from .loads import TCS2018, rename_default_patterns, classify_load_patterns, set_mass_source, create_H_spectrum_functions, create_V_spectrum_functions
-from .points import interpolate_z, split_number, split_space, generate_pattern, generate_roof_points
-from .spectrum import Sae, Saed
-from .sections import SectionLibrary
-from .materials import MaterialManager
